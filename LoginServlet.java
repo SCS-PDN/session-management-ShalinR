549b2e00518a6eef5447bbb15a6266f47007ea6d
Shalin <107199776+ShalinR@users.noreply.github.com>
2025-05-02 15:33:12 +0530

    Completed Course Registration System

--- LoginServlet.java
+++ LoginServlet.java
@@ -1,19 +1,41 @@
 import java.io.IOException;
+import java.util.HashMap;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
 
 @WebServlet("/LoginServlet")
 public class LoginServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+    private static final HashMap<String, String> users = new HashMap<>();
+
+    public void init() throws ServletException {
+        users.put("student1", "pass1");
+        users.put("student2", "pass2");
+        users.put("admin", "admin123");
+    }
+	
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
             throws ServletException, IOException {
-        // TODO: Implement login logic
-        // 1. Get username & password from request
-        // 2. Validate credentials (hardcode a few users)
-        // 3. If valid:
-        //    - Create session
-        //    - Store username in cookie
-        //    - Redirect to DashboardServlet
-        // 4. If invalid, redirect back to login.html
+        
+		String username=request.getParameter('username');
+		String password= request.getParameter('password');
+		
+		
+        if (users.containsKey(username) && users.get(username).equals(password)) {
+
+     
+        HttpSession session = request.getSession();
+        session.setAttribute("username", username);
+
+        Cookie cookie = new Cookie("username", username);
+        cookie.setMaxAge(60 * 60); 
+        response.addCookie(cookie);
+        response.sendRedirect("DashboardServlet");
+        }else {
+        	response.sendRedirect('login.html')
+        }
     }
 }
